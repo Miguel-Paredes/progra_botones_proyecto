f3d35b5aa12a1c57737c29bff81c53553f0167bd
AzureAD\10800 <os.miguelparedes@gmail.com>
2023-08-23 19:33:30 -0500

    update

--- src/conexion.java
+++ src/conexion.java
@@ -8,55 +8,49 @@ public class conexion {
     private JPanel JPanel;
     private JPasswordField contrasenia;
     private JButton verificarButton;
+    private JComboBox<String> comboBox1;
     static String DB_URL = "jdbc:mysql://localhost/medical";
     static String USER = "root";
     static String PASS = "root";
     static String QUERY = "SELECT * FROM Usuario";
     static String veriusu;
     static String vericontra;
+    static String selec;
     static String usu;
     static String contra;
+    static String seleccionar;
 
     public conexion() {
         verificarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                usu = usuario.getText().trim();
-                contra = new String(contrasenia.getPassword()).trim();
-                System.out.println(usu);
-                System.out.println(contra);
+                verificarConexion();}});}
 
-                try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
-                     Statement stmt = conn.createStatement();
-                     ResultSet rs = stmt.executeQuery(QUERY)) {
-
-                    while (rs.next()) {
-                        if (usu.equals(rs.getString("idUsuario"))) {
-                            veriusu = rs.getString("idUsuario");
-                        }
-                        if (contra.equals(rs.getString("contraseña"))) {
-                            vericontra = rs.getString("contraseña");
-                        }
-                    }
-                } catch (SQLException ex) {
-                    ex.printStackTrace();
-                }
-
-                System.out.println("El usuario es: " + veriusu);
-                System.out.println("La contraseña es: " + vericontra);
-            }
-        });
-    }
+    public void verificarConexion(){
+        seleccionar = (String) comboBox1.getSelectedItem();
+        usu = usuario.getText().trim();
+        contra = new String(contrasenia.getPassword()).trim();
+        try (Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(QUERY)) {
+            while (rs.next()) {
+                if (usu.equals(rs.getString("idUsuario"))) {
+                    veriusu = rs.getString("idUsuario");}
+                if (contra.equals(rs.getString("contraseña"))) {
+                    vericontra = rs.getString("contraseña");}
+                if (seleccionar.equals(rs.getString("tipoUsuario"))) {
+                    selec = rs.getString("tipoUsuario");}}}
+        catch (SQLException ex) {
+            ex.printStackTrace();}
+        System.out.println("El usuario es: " + veriusu);
+        System.out.println("La contraseña es: " + vericontra);
+        System.out.println("El cargo es: " + selec);}
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                JFrame frame = new JFrame("Conexion");
-                frame.setContentPane(new conexion().JPanel);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.pack();
-                frame.setVisible(true);
-            }
-        });
+        JFrame frame = new JFrame("conexion");
+        frame.setContentPane(new conexion().JPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
